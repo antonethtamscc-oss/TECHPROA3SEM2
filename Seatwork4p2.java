7d2bdb2d2fdffe674229db4defa98ad579a87845
antonethtamscc-oss <antonethtam.scc@gmail.com>
2026-02-10 17:36:39 +0800

    Yes

---
+++ Seatwork4p2.java
@@ -0,0 +1,21 @@
+public class ShapeAreaCalculator {
+
+    // Method for square
+    public int calculateArea(int side) {
+        return side * side;
+    }
+
+    // Method for rectangle (overloaded)
+    public double calculateArea(double length, double width) {
+        return length * width;
+    }
+
+    public static void main(String[] args) {
+        ShapeAreaCalculator calc = new ShapeAreaCalculator();
+
+        int square = calc.calculateArea(5);        // sample value
+        double rectangle = calc.calculateArea(4, 3); // sample value
+
+        System.out.println("Area of Square: " + square);
+        System.out.println("Area of Rectangle: " + rectangle);
+    }
